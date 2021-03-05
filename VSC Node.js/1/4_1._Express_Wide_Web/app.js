05a4208ccde243293d5475e9e17627be3a685b95
Bjergu <Bjergu@gmail.com>
2021-03-05 15:04:51 +0100

    anothercommit2

---
+++ VSC Node.js/1/4_1._Express_Wide_Web/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+
+    res.sendFile(__dirname + "/public/welcome.html");
+});
+
+
+
+app.listen(8080, (error) => {
+    if (error) {
+        console.log(error);
+    }
+    console.log("Server is running on port", 8080)
+});
